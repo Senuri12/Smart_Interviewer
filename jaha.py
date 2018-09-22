621b57ab9a92ce2eeaa13d0931764c8e1cb093da
Wick94 <ueetest8@gmail.com>
2018-09-22 16:03:36 +0530

    vikz_22/09_4.03pm

--- jaha.py
+++ jaha.py
@@ -10,17 +10,17 @@ MainQuestionGenerator.startsession()
 
 
 
-#
-# from Controller import SpeachToText
-#
-#
-# q="java  overriding object  technical"
-#
-# # subsection = input("enter sub area : \n")
-# # typer = input("enter type : \n")pickling
-# value = []
-# value = SpeachToText.validation("mocking", "nontechnical","nonested","question2")
-# print(value[1])
+
+from Controller import SpeachToText
+
+
+q="java  overriding object  technical"
+
+# subsection = input("enter sub area : \n")
+# typer = input("enter type : \n")pickling
+value = []
+value = SpeachToText.validation("mocking", "nontechnical","nonested","question2")
+print(value[1])
 
 #
 # import os
